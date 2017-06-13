da55dc313d3171cf2ffbf603beac95ca912a7649
Ruben Anton <rubocoptero@gmail.com>
2017-06-13 17:45:15 +0200

    Set up environment

---
+++ ruby/spec/spec_helper.rb
@@ -0,0 +1,5 @@
+require 'simplecov'
+
+SimpleCov.start
+
+require_relative '../trip/trip_service'
---
+++ ruby/spec/trip_service_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe TripService do
+  it 'does something' do
+  end
+end
